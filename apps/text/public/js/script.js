2590f68ea1047fddfe4dda94af0fb93fefb7b5f3
Aaron Yoshitake <airandfingers@gmail.com>
2013-06-24 23:25:08 -0700

    Added settings: fullscreen upon page load, default content

--- apps/text/public/js/script.js
+++ apps/text/public/js/script.js
@@ -2,12 +2,17 @@
 
 var editor = new EpicEditor({
   clientSideStorage: false
+, useNativeFullscreen: false
 , file: {
     autoSave: 1000
+  , defaultContent: '#EpicEditor\nThis is some default content. Go ahead, _change me_.'
   }
 }).load();
 
-editor.on('autosave', function(contents) {
-  console.log('autosaving:', contents);
-  $.post('/save', { contents: contents });
-});
+editor
+  .enterFullscreen()
+  .focus()
+  .on('autosave', function(contents) {
+    console.log('autosaving:', contents);
+    $.post('/save', { contents: contents });
+  });
