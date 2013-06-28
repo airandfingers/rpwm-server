d786cbd797029e93ad187ead938b8e8d50211f0d
bobbyinfj <robertnakano@gmail.com>
2013-06-28 00:05:37 -0700

    Added lightbox for chart. Changed copy for chart. Added slideshow for index.

--- apps/ets_tutoring_2/public/js/script.js
+++ apps/ets_tutoring_2/public/js/script.js
@@ -18,3 +18,50 @@ $(document).ready(function() {
 		$('.nav-pills li a[href="' + hash + '"]').tab('show');
 	}
 });
+
+//lightbox
+$('a.lightbox').click(function(e) {
+  //hide scrollbars!
+  $('body').css('overflow-y', 'hidden');
+
+  $('<div id="overlay"></div>')
+    .css('top', $(document).scrollTop())
+    .css('opacity', '0')
+    .animate({'opacity': '0.5'}, 'slow')
+    .appendTo('body');
+
+  $('<div id="lightbox"></div>')
+    .hide()
+    .appendTo('body');
+
+  $('<img />')
+    .attr('src', $(this).attr('href'))
+    .load(function() {
+      positionLightboxImage();
+    })
+    .click(function() {
+      removeLightbox();
+    })
+    .appendTo('#lightbox');
+
+  return false;
+});
+
+function positionLightboxImage() {
+  var top = ($(window).height() - $('#lightbox').height()) / 2;
+  var left = ($(window).width() - $('#lightbox').width()) / 2 ;
+  $('#lightbox')
+    .css({
+      'top': top + $(document).scrollTop(),
+      'left': left
+    })
+    .fadeIn();
+}
+
+function removeLightbox() {
+  $('#overlay, #lightbox')
+    .fadeOut('slow', function() {
+      $(this).remove();
+      $('body').css('overflow-y', 'auto'); //show scrollbars
+    });
+}
