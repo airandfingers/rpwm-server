c00dce32772a1c252b5cb1beb7c90695a270ee09
Aaron Yoshitake <airandfingers@gmail.com>
2014-09-17 18:10:44 -0700

    Tweak click-on-arrow directive - comments and disable when link is hidden

--- apps/trolls_goals/public/js/directives.js
+++ apps/trolls_goals/public/js/directives.js
@@ -90,22 +90,29 @@ directivesModule.directive('clickOnArrow', function($document, $timeout) {
   , up: 38
   , right: 39
   , down: 40
-  };
+  }; // JavaScript key codes for each arrow
   return function(scope, element, attrs) {
+    // translate from element attribute, e.g. click-on-arrow="left", to key code
     var key_code = directions_to_key_codes[attrs.clickOnArrow]
+      // this handler will be called for all keydown and keypress events
       , handler = function(e) {
-      var tag_name = e.target.tagName.toUpperCase();
-      if (tag_name === 'INPUT' || tag_name === 'TEXTAREA') {
-        return;
+      if (e.which !== key_code) {
+        return; // key pressed wasn't the one we want
       }
-      if (e.which === key_code) {
-        $timeout(function () {
-          element.click();
-        });
+      if (element.is(':hidden')) {
+        return; // link is hidden, meaning it shouldn't be clicked
+      }
+      var focused_tag_name = e.target.tagName.toUpperCase();
+      if (focused_tag_name === 'INPUT' || focused_tag_name === 'TEXTAREA') {
+        return; // user probably meant to navigate within the focused text box
+      }
+      $timeout(function() {
+        element.click();
+      });
 
-        e.preventDefault();
-      }
+      e.preventDefault();
     };
+    // listen for all keydown and keypress events at the top level ($document)
     $document.bind('keydown keypress', handler);
     // unbind handler, since body persists beyond element
     scope.$on('$destroy', function() {
